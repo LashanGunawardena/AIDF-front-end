5ce86b32c67027eba56ccfcf3f78418d49fc7ecf
lashan <ravijalashangunawardena@gmail.com>
2025-04-10 22:31:43 +0530

    added shadcn ui components

--- aidf-front-end/src/pages/home/components/jobSection.jsx
+++ aidf-front-end/src/pages/home/components/jobSection.jsx
@@ -2,13 +2,43 @@ import React from 'react'
 import JobCard from '@/components/shared/JobCard'
 
 const jobSection = () => {
-  return (
-    <section className='py-8'>
-        <div className='mt-4 flex flex-col gap-y-8'>
-            <JobCard/>
-        </div>
-    </section>
-  )
+    const jobs = [
+        {
+            _id: '1',
+            title: 'Software Engineer',
+            type: 'Full-time',
+            location: 'New York, NY',
+        },
+        {
+            _id: '2',
+            title: 'Data Scientist',
+            type: 'Part-time',
+            location: 'San Francisco, CA',
+        },
+        {
+            _id: '3',
+            title: 'Product Manager',
+            type: 'Contract',
+            location: 'Remote',
+        },
+    ];
+
+    return (
+        <section className='py-8'>
+            <div className='mt-4 flex flex-col gap-y-8'>
+                {
+                    jobs.map((job) => (
+                       <JobCard 
+                            title={job.title} 
+                            _id={job._id} 
+                            type={job.type} 
+                            location={job.location}
+                        />
+                    ))
+                }
+            </div>
+        </section>
+    );
 }
 
 export default jobSection
